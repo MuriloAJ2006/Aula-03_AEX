512bf66ddb076ea08ddc8ddf768c110f488bb5b3
Murilo Jorge <murilojorge62@gmail.com>
2026-09-12 16:26:00 -0300

    Main: Inicializa o Get/Post na Web e a janela Swing - Comentada

--- src/main/java/aex/aula3/Aula3Application.java
+++ src/main/java/aex/aula3/Aula3Application.java
@@ -1,5 +1,6 @@
 package aex.aula3;
 
+// Imports do Spring Boot
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,13 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Aula3Application {
 
 	public static void main(String[] args) {
-                System.setProperty("java.awt.headless", "false");
-		SpringApplication.run(Aula3Application.class, args);
-                
-                java.awt.EventQueue.invokeLater(() -> {
-                    JogoTela tela = new JogoTela();
-                    tela.exibirJanela();
-                });
-	}
+            
+            // Chamadas da Tela e da Aplicacao Spring
+            System.setProperty("java.awt.headless", "false");
+            SpringApplication.run(Aula3Application.class, args);
 
-}
+            // Cria e invoca a Tela
+            java.awt.EventQueue.invokeLater(() -> {
+
+                JogoTela tela = new JogoTela();
+                tela.exibirJanela();
+
+            }); // FIM DO INVOCADOR
+                
+	} // FIM DA MAIN
+
+} // FIM DA CLASSE
