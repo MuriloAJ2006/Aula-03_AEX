f0403aa2bef5432fa2b16fc36dc2ff49af3114d7
Murilo Jorge <murilojorge62@gmail.com>
2026-09-12 16:22:03 -0300

    Criação da Classe Personagem - Comentada

--- src/main/java/aex/aula3/Personagem.java
+++ src/main/java/aex/aula3/Personagem.java
@@ -7,14 +7,16 @@ public class Personagem {
     public int vida;
     
     // CONSTRUTORES:
+    // Construtor para o funcionamento do Get/Post na Web
     public Personagem () {
         
     } // FIM DO CONSTRUTOR 1
     
+    // Construtor para o funcionamento via terminal/janela swing
     public Personagem (String nomeRecebido) {
         
-        this.nome = nomeRecebido;
-        this.vida = 100;
+        this.nome = nomeRecebido;   // Faz com que o heroi receba o nome digitado
+        this.vida = 100;            // Determina previamente que a vida eh 100 ao criar
         
     } // FIM DO CONSTRUTOR 2
     
